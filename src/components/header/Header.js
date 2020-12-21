edeefecaca6fa80e97bcd05e17c58a9f12c07b3d
maksLazarchuk <maks.lazarchyk@gmailcom>
2020-12-21 23:43:24 +0200

    header link

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,6 +6,11 @@ import bag from "../../media/bag.png";
 import docu from "../../media/docu.png";
 
 function Header({ changeActivePages, activePages }) {
+  function addClassIfActive(index) {
+    if (index === activePages) return "active";
+    return;
+  }
+
   return (
     <div className="App">
       <header id="App-header">
@@ -16,29 +21,25 @@ function Header({ changeActivePages, activePages }) {
             </a>
           </div>
           <div className="links">
-            <div
-              className={
-                "profile link-box" + (activePages === 0 ? " active" : "")
-              }
-            >
+            <div className={"profile link-box " + addClassIfActive(0)}>
               <a href="#" onClick={() => changeActivePages(0)}>
                 <img src={user} alt="user" />
                 PROFILE
               </a>
             </div>
-            <div className="skills link-box">
+            <div className={"skills link-box " + addClassIfActive(1)}>
               <a href="#" onClick={() => changeActivePages(1)}>
                 <img src={circles} alt="circles" />
                 SKILLS
               </a>
             </div>
-            <div className="works link-box">
+            <div className={"works link-box " + addClassIfActive(2)}>
               <a href="#" onClick={() => changeActivePages(2)}>
                 <img src={bag} alt="bag" />
                 WORKS
               </a>
             </div>
-            <div className="resume link-box">
+            <div className={"resume link-box " + addClassIfActive(3)}>
               <a href="#" onClick={() => changeActivePages(3)}>
                 <img src={docu} alt="docu" />
                 RESUME
