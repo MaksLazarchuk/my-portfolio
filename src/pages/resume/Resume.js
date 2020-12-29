7f2dd834b31eddaadd31e738efe8182fbe0d391c
maksLazarchuk <maks.lazarchyk@gmailcom>
2020-12-29 13:59:27 +0200

    pages resume

--- src/pages/resume/Resume.js
+++ src/pages/resume/Resume.js
@@ -1,8 +1,19 @@
 import "./Resume.css";
+import download from "../../media/download.png";
 
 function Resume() {
   return (
-    <div className="resume"></div>
+    <div className="resume-content">
+      <h1 className="my-resume">MY RESUME</h1>
+      <p className="resume-text">
+        You can download my resume for your references, and I hope that we can
+        meet very soon
+      </p>
+      <button className="button-download">
+        <img className="download" src={download} alt="download" />
+        DOWNLOAD RESUME
+      </button>
+    </div>
   );
 }
 
